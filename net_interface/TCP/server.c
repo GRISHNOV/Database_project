d03840885ee62171bb5e526883992c7cd6e0632a
GRISHNOV <grishnov98@mail.ru>
2019-05-25 14:23:31 +0300

    net up

--- net_interface/TCP/server.c
+++ net_interface/TCP/server.c
@@ -1,7 +1,4 @@
-// It is possible to fork() main process for each client to allow each client to use it at the same time.
-//db project
-/* A simple echo TCP server */
-
+//db project server net interface
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -14,36 +11,44 @@
 
 int main()
 {
-    int    sockfd, newsockfd, clilen, n;
-    char   line[1000];
+    int sockfd, newsockfd, clilen, n;
+    char line[1000];
     struct sockaddr_in servaddr, cliaddr;
 
     bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family      = AF_INET;
-    servaddr.sin_port        = htons(51000);
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(51000);
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         printf("Can\'t create socket, errno = %d\n", errno);
         exit(1);
     }
 
-    if(bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
         printf("Can\'t bind socket, errno = %d\n", errno);
         close(sockfd);
         exit(1);
     }
 
-    if(listen(sockfd, 5) < 0){
+    if (listen(sockfd, 5) < 0)
+    {
         printf("Can\'t change state of socket to listen state, errno = %d\n", errno);
         close(sockfd);
         exit(1);
     }
 
-    while(1){
+    printf("\n*************************************************************\n");
+    printf("\nSERVER UP! START WORK!\n");
+
+    while (1)
+    {
         clilen = sizeof(cliaddr);
 
-        if( (newsockfd = accept(sockfd, (struct sockaddr *) &cliaddr, &clilen)) < 0){
+        if ((newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen)) < 0)
+        {
             printf("Can\'t accept connection, errno = %d\n", errno);
             close(sockfd);
             exit(1);
@@ -51,26 +56,35 @@ int main()
 
         printf("Connection with %s\n", inet_ntoa(cliaddr.sin_addr));
 
-        bzero(line,1000);
-        while( (n = read(newsockfd, line, 1000)) > 0){
-            printf("%s\n",line);
-            if( (n = write(newsockfd, line, strlen(line)+1)) < 0){
+        bzero(line, 1000);
+        while ((n = read(newsockfd, line, 1000)) > 0)
+        {
+            printf("%s\n", line);
+
+            FILE *file;
+            file = fopen("./server_receved_temp.txt", "w");
+            fprintf(file, "%s", line);
+            fclose(file);
+
+            if ((n = write(newsockfd, line, strlen(line) + 1)) < 0)
+            {
                 printf("Can\'t write, errno = %d\n", errno);
                 close(sockfd);
                 close(newsockfd);
                 exit(1);
             }
-            bzero(line,1000);
+            bzero(line, 1000);
         }
 
-        if(n < 0){
+        if (n < 0)
+        {
             printf("Can\'t read, errno = %d\n", errno);
             close(sockfd);
             close(newsockfd);
             exit(1);
         }
         close(newsockfd);
-        printf("Connection closed\n");
+        printf("Connection closed %s\n", inet_ntoa(cliaddr.sin_addr));
     }
     return 0;
 }
--- net_interface/TCP/user.c
+++ net_interface/TCP/user.c
@@ -1,5 +1,4 @@
-/* A simple echo TCP client */
-//db project
+//db project user net interface
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -13,56 +12,75 @@
 int main(int argc, char **argv)
 {
 
-    int    sockfd, n, i;
-    char   sendline[1000],recvline[1000];
+    int sockfd, n, i;
+    char sendline[1000], recvline[1000];
     struct sockaddr_in servaddr;
+    //char close_line_command[16] = "close";
+    //int close_connection_flag = 0;
 
-    if(argc != 2){
+    if (argc != 2)
+    {
         printf("Usage: a.out <IP address>\n");
         exit(1);
     }
 
-    bzero(sendline,1000);
-    bzero(recvline,1000);
+    bzero(sendline, 1000);
+    bzero(recvline, 1000);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port   = htons(51000);
+    servaddr.sin_port = htons(51000);
 
-    if(inet_aton(argv[1], &servaddr.sin_addr) == 0){
+    if (inet_aton(argv[1], &servaddr.sin_addr) == 0)
+    {
         printf("Invalid IP address\n");
         exit(1);
     }
 
-    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
         printf("Can\'t create socket, errno = %d\n", errno);
         exit(1);
     }
 
-    if(connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
         printf("Can\'t connect, errno = %d\n", errno);
         close(sockfd);
         exit(1);
     }
 
-    for(i=0; i<3; i++){
-        printf("String => ");
+    printf("\n*************************************************************\n");
+    printf("\nCONNECTION OK. WRITE q! to exit\n");
+    printf("\nSELECT <...> from <...>\n");
+
+    while (1)
+    {
+        printf("COMMAND => ");
         fflush(stdin);
         fgets(sendline, 1000, stdin);
 
-        if( (n = write(sockfd,sendline, strlen(sendline)+1)) < 0){
+        if (sendline[0] == 'q' && sendline[1] == '!')
+        {
+            close(sockfd);
+            break;
+        }
+
+        if ((n = write(sockfd, sendline, strlen(sendline) + 1)) < 0)
+        {
             printf("Can\'t write, errno = %d\n", errno);
             close(sockfd);
             exit(1);
         }
 
-        if ( (n = read(sockfd,recvline, 1000)) < 0){
+        if ((n = read(sockfd, recvline, 1000)) < 0)
+        {
             printf("Can\'t read, errno = %d\n", errno);
             close(sockfd);
             exit(1);
         }
         printf("%s", recvline);
     }
-    close(sockfd);
+    //close(sockfd);
     return 0;
 }
